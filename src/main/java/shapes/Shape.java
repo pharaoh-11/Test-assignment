8eeae2af7ca494721ba6263dfa5fdefec8799697
Vadym Kukurika <vadymku@tahometer.com>
2019-04-23 13:34:26 +0300

    Create an abstract class

---
+++ src/main/java/shapes/Shape.java
@@ -0,0 +1,13 @@
+package shapes;
+
+public abstract class Shape {
+    protected abstract void getName();
+    protected abstract double getArea();
+    protected abstract String getMeasuringItem();
+    protected abstract void getColor();
+
+
+    public void draw() {
+
+    }
+}
